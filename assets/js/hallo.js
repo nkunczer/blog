764b5b30adb09aa3802a1656cda20ac9682821cd
nkunczer <n.kunczer@gmail.com>
2023-07-08 21:33:01 +0200

    add JS script

---
+++ assets/js/hallo.js
@@ -0,0 +1,8 @@
+(function() {
+   
+    function sayHello() {
+        console.log('Hallo Christian! Wenn du das hier lesen kannst, funktioniert mein JS!');
+    }
+
+    document.addEventListener('DOMContentLoaded', sayHello);
+})();
